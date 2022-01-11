aa2ec807fea67a25d01c8f8d559d7d8b32cebbca
xiaozhe <tr1dent_sz@outlok.com>
2022-01-11 11:09:02 +0800

    add 3 files

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+# author:didi
+# contact: sunzheyu
+# datetime:2022/1/11 11:06 上午
+# software: PyCharm
+
+"""
+文件说明：
+"""
---
+++ api/views/userlogin.py
@@ -0,0 +1,11 @@
+# author:didi
+# contact: sunzheyu
+# datetime:2022/1/11 11:07 上午
+# software: PyCharm
+
+"""
+文件说明：
+"""
+
+def login():
+    print("login success")
